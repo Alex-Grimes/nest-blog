a34a54006dd016883bf521ce076360f1a64c6081
alex <alexgrimes.se@gmail.com>
2024-01-31 17:35:24 -0500

    generated additional blog files with nest cli

--- blog-backend/src/app.module.ts
+++ blog-backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest-blog', {
       useNewUrlParser: true,
     }),
+    BlogModule,
   ],
   controllers: [AppController],
   providers: [AppService],
